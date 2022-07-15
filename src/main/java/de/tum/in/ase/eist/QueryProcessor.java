3f7a22397ca5acb21081ebf0bfb108bcbdd6e9e3
ge63qow <sq.chen@tum.de>
2022-07-15 15:28:14 +0200

    extended

--- src/main/java/de/tum/in/ase/eist/QueryProcessor.java
+++ src/main/java/de/tum/in/ase/eist/QueryProcessor.java
@@ -2,6 +2,8 @@ package de.tum.in.ase.eist;
 
 import org.springframework.stereotype.Service;
 
+import java.util.Arrays;
+
 @Service
 public class QueryProcessor {
 
@@ -12,9 +14,28 @@ public class QueryProcessor {
                     "English poet, playwright, and actor, widely regarded as the greatest " +
                     "writer in the English language and the world's pre-eminent dramatist.";
         } else if (query.contains("name")) {
-           return "MyTeam";
-        } else { // TODO extend the programm here
-            return "";
+            return "MyTeam";
+        } else if (query.contains("plus")) { // TODO extend the programm here
+            String sub = query.substring(8);
+            String[] nums = sub.split("plus");
+            int a = Integer.parseInt(nums[0]);
+            int b = Integer.parseInt(nums[1]);
+            return Integer.toString(a + b);
+        } else {
+            String sub = query.substring(query.indexOf(":") + 1);
+            String[] nums = sub.split(",");
+            int[] n = new int[nums.length];
+            for (int i = 0; i < nums.length; i++) {
+                n[i] = Integer.parseInt(nums[i]);
+            }
+
+            int max = 0;
+            for (int i = 0; i < n.length; i++) {
+                if (n[i] > max) {
+                    max = n[i];
+                }
+            }
+            return Integer.toString(max);
         }
     }
 }
